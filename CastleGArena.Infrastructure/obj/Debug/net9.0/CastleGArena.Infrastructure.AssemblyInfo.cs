2e3dfbdee5309d8b30dd8c31745d1fa5ef3e52c1
Nischitha <nischithanishu18@gmail.com>
2026-05-28 21:16:44 +0530

    Still more changes for name change

--- CastleGArena.Infrastructure/obj/Debug/net9.0/CastleGArena.Infrastructure.AssemblyInfo.cs
+++ CastleGArena.Infrastructure/obj/Debug/net9.0/CastleGArena.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CastleGArena.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd279208082e3f1cf0b94d43461cea24dac3a6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1c179e21596089d8a5ed72888d717578a799f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CastleGArena.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CastleGArena.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
